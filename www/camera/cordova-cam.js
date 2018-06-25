e50b98747ef881eb7a69820e5b55497eae4b021b
Nancho07 <hernan@imixdigital.com>
2018-06-25 12:43:59 -0600

    Add files via upload

--- www/camera/cordova-cam.js
+++ www/camera/cordova-cam.js
@@ -1,8 +1,9 @@
 (function(){
 'use strict';
-var pictureSource;
-var destinationType;
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
 var imageID;
+// Cordova is ready to be used!
 function onDeviceReady() {
     if (!navigator.camera) {
       throw new Error('Cordova camera plugin required to access hardware camera.');
@@ -13,23 +14,71 @@ function onDeviceReady() {
       throw new Error('Cordova File Transfer required.');
     }
 } 
+// Wait for Cordova to connect with the device
 document.addEventListener("app.Ready", onDeviceReady, false);
+// Called when a photo is successfully retrieved (photo taken)
 function onPhotoDataSuccess(imageData) {
-    delServidor({mensaje:"Capturando imagen" + imageData});
   var imageNode = $(imageID);
-  var photo = imageData; //'data:image/jpeg;base64,' + imageData;
-  imageNode.attr('src', photo);
+  //var photo = imageData; //'data:image/jpeg;base64,' + imageData;
+  imageNode.attr('src', imageData);
     function win(r) {
         //console.log("Code = " + r.responseCode);
     }    
     function fail(error) {
         delServidor({mensaje:"An error has occurred: Code = " + error.code,success:false});
     }
-    var uri = encodeURI($("#inputrootUrl").val());
+    var uri = encodeURI(inputrootUrl);
     var fileURL = imageData;
     var options = new FileUploadOptions();
         options.fileKey="file";
-    var params={}; 
+    
+    /************** Hernan proceso **************/
+    function takeSnapshot(){
+        /*var hidden_canvas = document.querySelector('canvas'),
+            context = hidden_canvas.getContext('2d');
+        var width = (imageNode.width()),
+            height = (imageNode.height());
+        if (width && height) {            
+            // Configuramos el canvas con las mismas dimensiones que el video
+            hidden_canvas.width = width;
+            hidden_canvas.height = height;
+            // Hacemos una copia
+            context.drawImage(imageNode, 0, 0, width, height);
+            // Convertimos la imagen del canvas en datarurl
+            //console.log(hidden_canvas.width ,"&&", hidden_canvas.height);
+            return hidden_canvas.toDataURL('image/png');
+        }*/
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+        ctx.drawImage(imageNode, 10, 10);
+        return c.toDataURL();
+    } 
+    /****************************************************/
+    var datosPaciente={}; 
+        
+        datosPaciente.paciente_k=$("#pre_paciente_k").val();
+        datosPaciente.nombres	=$("#pre_nombre").val();
+        datosPaciente.apellidos	=$("#pre_apellido").val();
+        datosPaciente.num_id	=$("#pre_identidad").val();
+        datosPaciente.prefix	=$("#pre_prefix").val();
+        datosPaciente.fecha_modificacion = moment(new Date()).format('YYYY-MM-DD');
+        //console.log("Imagen - Paciente 1-> ",datosPaciente)
+        datosPaciente.foto = takeSnapshot();
+        //console.log("Imagen - Paciente-> ",datosPaciente)
+        socket.emit("imagenPaciente",datosPaciente); 
+    if(datosPaciente.paciente_k != 0){  
+        if(datosPaciente.nombres!="" && datosPaciente.apellidos!="" && datosPaciente.num_id!=""){
+          
+        }else{
+          delServidor({mensaje:"No se han completado los datos obligatoros",success:false});
+    	}
+    }else{
+        delServidor({mensaje:"Para guardar la foto debe crear el usuario antes.",success:false});
+        alertas.contenido="Para guardar la foto debe crear el usuario antes.";
+    }
+    //$(".app").css("display",true);
+    /********************************************/
+    /*var params={}; 
         params.paciente_k=$("#pre_paciente_k").val();
         params.nombres	=$("#pre_nombre").val();
         params.apellidos	=$("#pre_apellido").val();
@@ -44,11 +93,13 @@ function onPhotoDataSuccess(imageData) {
             FT=FT.replace(/%/g,"_");
             options.fileName=FT+".jpg";
         }
-        options.mimeType="image/jpeg"; 
-        options.directorio="imgPanta"; 
-        var headers={'headerParam':'headerValue'};    
-        options.headers = headers;
-    socket.emit("imagenPaciente",options); 
+        socket.emit("imagenPaciente",options); 
+    
+    options.mimeType="image/jpeg"; 
+    options.directorio="imgPanta"; 
+    var headers={'headerParam':'headerValue'};    
+    options.headers = headers;
+    */
     /*var ft = new FileTransfer();
     ft.onprogress = function(ProgressEvent) {
         if (ProgressEvent.lengthComputable) {
@@ -84,6 +135,7 @@ function onPhotoURISuccess(imageURI) {
 // A button will call this function
 function capturePhoto(uib_id) {
   imageID = uib_id || '#imgPre';
+  // Take picture using device camera and retrieve image as base64-encoded string
   if(!navigator.camera) {
     onFail('Missing the Cordova camera plugin');
   }
@@ -99,7 +151,7 @@ function captureAndSavePhoto(uib_id) {
   if(!navigator.camera) {
     onFail('Missing the Cordova camera plugin');
   }
-  imageID = uib_id || '#IMAGE';
+  imageID = uib_id || '#imgPre';
   //desinationType and saveToPhotoAlbum must be set correctly to save the photo to the camera roll
   var cameraOptions = {
     quality: 20,
@@ -112,13 +164,12 @@ window.captureAndSavePhoto = captureAndSavePhoto;
 
 // A button will call this function
 function capturePhotoEdit(uib_id) {
-  imageID = uib_id || '#IMAGE';
+  imageID = uib_id || '#imgPre';
   // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
     destinationType: destinationType.DATA_URL,sourceType: source });
 }
 window.capturePhotoEdit = capturePhotoEdit;
-
 // A button will call this function
 function getPhoto(source) {
   // Retrieve image file location from specified source
@@ -127,13 +178,12 @@ function getPhoto(source) {
     sourceType: source });//pictureSource
 }
 window.getPhoto = getPhoto;
-
 // Called if something bad happens.
 function onFail(message) {
   throw new Error('Camera failed: ' + message);
   alert('Error causado por: ' + message);
 }
- /*********************** Hernan ***********************/
+/*********************** Hernan ***********************/
 function getImage(source){
   var cameraOptions = {
     quality: 20,
@@ -141,7 +191,7 @@ function getImage(source){
     sourceType: source,
     saveToPhotoAlbum: false
   };
-    navigator.camera.getPicture(onPhotoDataSuccess, onCapturePhotoError,cameraOptions);
+  navigator.camera.getPicture(onPhotoDataSuccess, onCapturePhotoError,cameraOptions);
     
     /*var cameraOptions1 = {
     quality: 20,
@@ -153,7 +203,7 @@ function getImage(source){
 }
 window.getImage = getImage;
 function onCapturePhotoSuccess(imageURI){ 
-  var imageNode = document.getElementById('IMAGE');
+  var imageNode = document.getElementById('imgPre');
   var photo = imageURI;
   imageNode.src="data:image/jpeg;base64,"+photo;
 }
